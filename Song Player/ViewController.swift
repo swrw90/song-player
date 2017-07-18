bde7ee8f0806f57cff5d32b66eb48df360b6d4c8
Seth Watson <seth.watson1@gmail.com>
2017-07-17 22:51:39 -0600

    Initial Commit

---
+++ Song Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Song Player
+//
+//  Created by Seth Watson on 7/17/17.
+//  Copyright © 2017 Seth Watson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
